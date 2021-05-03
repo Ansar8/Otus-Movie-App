ade391a042c4390ab3597bacf351bf0f0b5e00cd
Ansar Kadyrov <j423252@gmail.com>
2021-05-03 18:18:38 +0300

    Add id field to Movie object.

--- app/src/main/java/com/example/otusmovieapp/Data.kt
+++ app/src/main/java/com/example/otusmovieapp/Data.kt
@@ -4,12 +4,12 @@ class Data {
 
     companion object {
         val movieList: ArrayList<Movie> = arrayListOf(
-                Movie("Godzilla", R.drawable.image1, isReviewed = false, isFavorite = false),
-                Movie("Raya", R.drawable.image2, isReviewed = false, isFavorite = false),
-                Movie("Justice League", R.drawable.image3, isReviewed = false, isFavorite = false),
-                Movie("Godzilla", R.drawable.image1, isReviewed = false, isFavorite = false),
-                Movie("Raya", R.drawable.image2, isReviewed = false, isFavorite = false),
-                Movie("Justice League", R.drawable.image3, isReviewed = false, isFavorite = false)
+                Movie(1, "Godzilla", R.drawable.image1, isReviewed = false, isFavorite = false),
+                Movie(2, "Raya", R.drawable.image2, isReviewed = false, isFavorite = false),
+                Movie(3, "Justice League", R.drawable.image3, isReviewed = false, isFavorite = false),
+                Movie(4, "Godzilla", R.drawable.image1, isReviewed = false, isFavorite = false),
+                Movie(5, "Raya", R.drawable.image2, isReviewed = false, isFavorite = false),
+                Movie(6, "Justice League", R.drawable.image3, isReviewed = false, isFavorite = false)
         )
     }
 }
