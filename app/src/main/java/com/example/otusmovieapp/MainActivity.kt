28cb5a636123dc601fe84d72d0cc81380c691614
Ansar Kadyrov <j423252@gmail.com>
2021-05-04 00:16:10 +0300

    Show custom dialog before user quits the app.

--- app/src/main/java/com/example/otusmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/otusmovieapp/MainActivity.kt
@@ -56,6 +56,10 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onBackPressed() {
+        ExitDialog().show(supportFragmentManager, "exitDialog")
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.favoritesItem -> {
